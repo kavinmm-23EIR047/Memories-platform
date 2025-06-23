283b503e0029e26287330604acc9a0d39354fbbf
KAVIN M M <156902492+kavinmm-23EIR047@users.noreply.github.com>
2025-06-23 13:15:24 +0530

    Update Contact.jsx

--- travel-frontend/src/components/Contact.jsx
+++ travel-frontend/src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://memories-bakend.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
