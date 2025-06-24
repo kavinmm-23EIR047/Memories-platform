ee18db73583bd3d2d6ac4da26efe2050fbefa289
KAVIN M M <156902492+kavinmm-23EIR047@users.noreply.github.com>
2025-06-24 22:27:20 +0530

    Update GalleryBusPage.jsx

--- travel-frontend/src/components/GalleryBusPage.jsx
+++ travel-frontend/src/components/GalleryBusPage.jsx
@@ -217,7 +217,7 @@ const GalleryBusPage = () => {
   return (
    <section className="pt-32 pb-16 px-6 md:px-12 bg-gray-100 scroll-mt-32">
   <div className="max-w-6xl mx-auto bg-white p-8 md:p-12 rounded-2xl shadow-2xl">
-    <h2 className="text-4xl md:text-5xl font-bold text-[#facc15] text-center mb-10 font-mui">
+    <h2 className="text-4xl md:text-5xl font-bold text-[#facc15] text-center mb-10 font-mai">
       {bus.name}
     </h2>
 
@@ -240,7 +240,7 @@ const GalleryBusPage = () => {
             <img
               src={img}
               alt={`Bus ${idx + 1}`}
-              className="w-full h-[500px] object-cover rounded-xl transform hover:scale-105 transition duration-300 ease-in-out font-mui"
+              className="w-full h-[500px] object-cover rounded-xl transform hover:scale-105 transition duration-300 ease-in-out font-mai"
             />
           </div>
         </SwiperSlide>
@@ -249,7 +249,7 @@ const GalleryBusPage = () => {
 
     {/* 🎥 Video Section */}
     <div className="mt-12">
-      <h3 className="text-3xl font-semibold text-gray-800 mb-6 font-mui text-center">
+      <h3 className="text-3xl font-semibold text-gray-800 mb-6 font-mai text-center">
         Bus Video:
       </h3>
 
